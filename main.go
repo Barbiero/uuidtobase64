9384dc093b718f958287822996fe22ce773dbecf
Pedro Barbiero <pedrobarbiero@gmail.com>
2022-09-23 15:06:33 -0300

    feat: read from stdin feat: trim input whitespace feat: "-r" flag to reverse the process

--- main.go
+++ main.go
@@ -2,8 +2,11 @@ package main
 
 import (
 	"encoding/base64"
+	"flag"
 	"fmt"
+	"io"
 	"os"
+	"strings"
 
 	"github.com/google/uuid"
 )
@@ -11,7 +14,8 @@ import (
 const USAGE = `Outputs a base64-encoded byte array for an UUID
 
 Usage:
-	uuidtobase64 [UUID]`
+	uuidtobase64 [UUID]                          receives an UUID string and outputs a base64-encoded string
+	uuidtobase64--reverse [base64_string]        receives a base64-encoded string and outputs an UUID string`
 
 type exitCode uint8
 
@@ -20,14 +24,50 @@ const (
 	ExitError
 )
 
-func prog(args []string) (output string, e exitCode) {
-	if len(args) == 0 || len(args[0]) == 0 {
+func prog(args []string, stdin io.Reader) (output string, e exitCode) {
+	flags := flag.NewFlagSet("uuidtobase64", flag.ContinueOnError)
+	reverse := flags.Bool("r", false, "reads a base64-encoded 16 byte array and outputs as an UUID string")
+	parseErr := flags.Parse(args)
+	if parseErr != nil {
 		return USAGE, ExitError
 	}
 
-	id, err := uuid.Parse(args[0])
+	var input string
+	if len(flags.Args()) == 0 {
+		in, _ := io.ReadAll(stdin)
+		input = strings.TrimSpace(string(in))
+	} else {
+		input = strings.TrimSpace(flags.Args()[0])
+	}
+	if len(input) == 0 {
+		return USAGE, ExitError
+	}
+
+	if *reverse {
+		return parseByteArrayToUUID(input)
+	}
+	return parseUUIDToByteArray(input)
+
+}
+
+func parseByteArrayToUUID(byteString string) (string, exitCode) {
+	byteSlice, err := base64.StdEncoding.DecodeString(byteString)
 	if err != nil {
-		return "could not parse uuid " + args[0] + ": " + err.Error(), ExitError
+		return "could not parse byte string " + byteString + ": " + err.Error(), ExitError
+	}
+
+	res, err := uuid.FromBytes(byteSlice)
+	if err != nil {
+		return "input byte string is not UUID compatible: " + err.Error(), ExitError
+	}
+
+	return res.String(), ExitSuccess
+}
+
+func parseUUIDToByteArray(uuidStr string) (string, exitCode) {
+	id, err := uuid.Parse(uuidStr)
+	if err != nil {
+		return "could not parse uuid " + uuidStr + ": " + err.Error(), ExitError
 	}
 
 	result := base64.StdEncoding.EncodeToString(id[:])
@@ -35,7 +75,11 @@ func prog(args []string) (output string, e exitCode) {
 }
 
 func main() {
-	output, code := prog(os.Args[1:])
-	fmt.Println(output)
+	output, code := prog(os.Args[1:], os.Stdin)
+	out := os.Stdout
+	if code != ExitSuccess {
+		out = os.Stderr
+	}
+	_, _ = fmt.Fprintln(out, output)
 	os.Exit(int(code))
 }
--- main_test.go
+++ main_test.go
@@ -1,10 +1,41 @@
 package main
 
 import (
+	"bytes"
+	"os"
 	"testing"
 )
 
-func TestProg(t *testing.T) {
+func Test_ParseFromStdin(t *testing.T) {
+	type testCase struct {
+		name             string
+		inputArgs        []string
+		stdinValue       string
+		expectedOutput   string
+		expectedExitCode exitCode
+	}
+	testCases := [...]testCase{
+		{"uuid to byte array", nil, "6f49a35b-5da9-4e92-bd13-5f7891845e09", "b0mjW12pTpK9E194kYReCQ==", ExitSuccess},
+		{"byte array to uuid", []string{"-r"}, "b0mjW12pTpK9E194kYReCQ==", "6f49a35b-5da9-4e92-bd13-5f7891845e09", ExitSuccess},
+		{"empty input and empty stdin", nil, "", USAGE, ExitError},
+	}
+	buffer := new(bytes.Buffer)
+	for _, tt := range testCases {
+		t.Run(tt.name, func(t *testing.T) {
+			t.Cleanup(buffer.Reset)
+			buffer.Write([]byte(tt.stdinValue))
+			output, code := prog(tt.inputArgs, buffer)
+			if output != tt.expectedOutput {
+				t.Errorf("Expected %q to be equal to %q", output, tt.expectedOutput)
+			}
+			if code != tt.expectedExitCode {
+				t.Errorf("Expected exitCode to be %d", tt.expectedExitCode)
+			}
+		})
+	}
+}
+
+func Test_ParseUUIDToByteArray(t *testing.T) {
 	type testCase struct {
 		name             string
 		inputArgs        []string
@@ -12,7 +43,7 @@ func TestProg(t *testing.T) {
 		expectedExitCode exitCode
 	}
 
-	testCases := []testCase{
+	testCases := [...]testCase{
 		{"no args", nil, USAGE, ExitError},
 		{"many args", []string{"there", "are", "many", "args"}, "could not parse uuid there: invalid UUID length: 5", ExitError},
 		{"one bad arg", []string{"bad-arg"}, "could not parse uuid bad-arg: invalid UUID length: 7", ExitError},
@@ -24,7 +55,38 @@ func TestProg(t *testing.T) {
 
 	for _, tt := range testCases {
 		t.Run(tt.name, func(t *testing.T) {
-			output, code := prog(tt.inputArgs)
+			output, code := prog(tt.inputArgs, os.Stdin)
+			if output != tt.expectedOutput {
+				t.Errorf("Expected %q to be equal to %q", output, tt.expectedOutput)
+			}
+			if code != tt.expectedExitCode {
+				t.Errorf("Expected exitCode to be %d", tt.expectedExitCode)
+			}
+		})
+	}
+}
+
+func Test_ParseByteArrayToUUID(t *testing.T) {
+	type testCase struct {
+		name             string
+		inputArgs        []string
+		expectedOutput   string
+		expectedExitCode exitCode
+	}
+
+	testCases := [...]testCase{
+		{"no args", nil, USAGE, ExitError},
+		{"many args", []string{"-r", "there", "are", "many", "args"}, "could not parse byte string there: illegal base64 data at input byte 4", ExitError},
+		{"one bad arg", []string{"-r", "bad-arg"}, "could not parse byte string bad-arg: illegal base64 data at input byte 3", ExitError},
+		{"valid base64", []string{"-r", "b0mjW12pTpK9E194kYReCQ=="}, "6f49a35b-5da9-4e92-bd13-5f7891845e09", ExitSuccess},
+		{"valid base64 v1", []string{"-r", "u9p0hNiaEeydZAJCrBIAAg=="}, "bbda7484-d89a-11ec-9d64-0242ac120002", ExitSuccess},
+		{"invalid base64", []string{"-r", "YmxhaAo="}, "input byte string is not UUID compatible: invalid UUID (got 5 bytes)", ExitError},
+		{"invalid base64", []string{"-abcd", ""}, USAGE, ExitError},
+	}
+
+	for _, tt := range testCases {
+		t.Run(tt.name, func(t *testing.T) {
+			output, code := prog(tt.inputArgs, os.Stdin)
 			if output != tt.expectedOutput {
 				t.Errorf("Expected %q to be equal to %q", output, tt.expectedOutput)
 			}
